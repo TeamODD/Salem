007918329f6cd4c536e529195507485281a1d860
th0079 <taehyun8681@gmail.com>
2026-01-16 21:50:37 +0900

    feat: 장전 버튼 누르면 마우스 변경

---
+++ Assets/Scripts/Test/CursorManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    public Texture2D GunTexture;
+    public Texture2D OriginalTexture;
+    public Vector2 Hotspot = Vector2.zero;
+    private bool _isOriginal = true;
+
+    public void SwitchCursor()
+    {
+        if (_isOriginal)
+        {
+            SetGunCursor();
+        }
+        else
+        {
+            SetOriginalCursor();
+        }
+    }
+
+    public void SetGunCursor()
+    {
+        Cursor.SetCursor(GunTexture, Hotspot, CursorMode.Auto);
+        _isOriginal = false;
+    }
+
+    public void SetOriginalCursor()
+    {
+        Cursor.SetCursor(OriginalTexture, Hotspot, CursorMode.Auto);
+        _isOriginal = true;
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
